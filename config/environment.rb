2fab82fe0fb5c2866502284faa3402def4999486
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-20 20:41:19 +0500

    [103]config send confirmation email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,24 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+# config/environments/development.rb
+Rails.application.configure do
+  
+    # Action Mailer configuration
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: "172.67.133.34", # Change this to your SMTP server address
+      port: 587, # Change this to your SMTP server port
+      domain: "good.io", # Change this to your domain
+      user_name: "usamaqazi", # Change this to your SMTP username
+      password: "123456", # Change this to your SMTP password
+      authentication: "plain",
+      enable_starttls_auto: true
+    }
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default_url_options = { host: "localhost", port: 3000 } # Change to your development host and port
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :customers
 root to: 'customers#index'
+
    devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
