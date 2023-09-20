a002072e3aa832990f06857f439d73315053f9b1
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-20 20:42:14 +0500

    [103]error fix

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,22 +1,56 @@
 class CustomersController < ApplicationController
     def index
         @users = User.all
-      end
-    
-      def show
-        @user = User.find(params[:id])
-      end
-      def edit
-        @user = User.find(params[:id])
-      end
+    end
+    def new
+        @users = User.new
+    end
 
-      def destroy
-        @user = User.find(params[:id])
-        if @user.destroy
-          redirect_to root_path, notice: "User was successfully destroyed."
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "Successfully Added"
+            redirect_to root_path
         else
-          redirect_to root_path, alert: "Unable to destroy user."
+            render :new
         end
-      end
-      
+    end
+    
+    def show
+    @user = User.find(params[:id])
+    end
+
+
+    def edit
+    @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        
+        if @user.update(user_params)
+            flash[:success] = "Successfully Updated"
+            redirect_to root_path
+
+            
+        else
+            render :edit
+        end
+    end
+
+
+    def destroy
+    @user = User.find(params[:id])
+        if @user.destroy
+            redirect_to root_path, notice: "User was successfully destroyed."
+        else
+            redirect_to root_path, alert: "Unable to destroy user."
+        end
+    end
+
+
+    private
+        def user_params
+            params.require(:user).permit(:username, :email,:password, :password_confirmation)
+        end
 end
