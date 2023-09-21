fa8d759d550809038bf3a28cb79fbaf6762eee84
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-21 17:40:04 +0500

    [104]drop table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_21_102046) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_21_112812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,15 +37,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_21_102046) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "users_roles", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "role_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["role_id"], name: "index_users_roles_on_role_id"
-    t.index ["user_id"], name: "index_users_roles_on_user_id"
-  end
-
-  add_foreign_key "users_roles", "roles"
-  add_foreign_key "users_roles", "users"
 end
