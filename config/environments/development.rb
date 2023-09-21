3a99952e0744fbd9f147719859438d9e285c3768
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-21 17:39:21 +0500

    [104]config letter opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,17 +67,19 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.delivery_method = :smtp
-  ActionMailer::Base.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-  address: 'smtp.gmail.com',
-  port: 465, # Use the Mailtrap SMTP port
-  domain: 'gmail.com', # Your domain
-  user_name: 'usamaqazi',
-  password: '123456',
-  authentication: 'plain',
-  enable_starttls_auto: true
-}
+#   config.action_mailer.delivery_method = :smtp
+#   ActionMailer::Base.perform_deliveries = true
+#   config.action_mailer.smtp_settings = {
+#   address: 'smtp.gmail.com',
+#   port: 465, # Use the Mailtrap SMTP port
+#   domain: 'gmail.com', # Your domain
+#   user_name: 'usamaqazi',
+#   password: '123456',
+#   authentication: 'plain',
+#   enable_starttls_auto: true
+# }
+config.action_mailer.delivery_method = :letter_opener
+config.action_mailer.perform_deliveries = true
 
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
