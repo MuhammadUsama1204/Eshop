e83e4e55e00f7745448a9b9df83a1239dfd20dc2
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-22 15:16:59 +0500

    [104]add Usersrole and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,7 @@
 class User < ApplicationRecord
-  has_many :users_roles
+  has_many :users_roles, class_name: "UsersRole"
   has_many :roles, through: :users_roles
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  validates :password, format: { with: /[\!@#\$%\^&\*]/, message: "must include at least one special character" }
-  # enum role: {
-  #   admin: 'admin',
-  #   staff: 'staff',
-  #   customer: 'customer'
-  # }
-
- 
+  validates :password, format: { with: /[\!@#\$%\^&\*]/, message: "must include at least one special character" }, on: :create
 end
