3c0314e814c17ef777a5072469f90c480d811d11
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-22 11:34:38 +0500

    [104]error fixing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ApplicationRecord
+  has_many :users_roles
+  has_many :roles, through: :users_roles
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   validates :password, format: { with: /[\!@#\$%\^&\*]/, message: "must include at least one special character" }
-  enum role: {
-    admin: 'admin',
-    staff: 'staff',
-    customer: 'customer'
-  }
+  # enum role: {
+  #   admin: 'admin',
+  #   staff: 'staff',
+  #   customer: 'customer'
+  # }
 
  
 end
--- db/migrate/20230921112812_drop_users_roles_table.rb
+++
@@ -1,5 +0,0 @@
-class DropUsersRolesTable < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :users_roles
-  end
-end
