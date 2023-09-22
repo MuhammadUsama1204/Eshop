0ce99248f9c84d62df5b2593af7d8fdef81be09b
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-22 17:00:37 +0500

    [104]add by default role Customer

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   validates :password, format: { with: /[\!@#\$%\^&\*]/, message: "must include at least one special character" }, on: :create
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.users_roles.build(role_id: Role.find_by(role: 'Customer').id)
+  end
 end
