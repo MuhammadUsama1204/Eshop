f214fce9357980c80c366bcfaf37bbb1bdf9bb62
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-22 11:56:08 +0500

    [103] fixing and rmovies extra lines

--- Gemfile
+++ Gemfile
@@ -53,6 +53,7 @@ group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
   gem "devise"
   gem "byebug"
+  gem "mailcatcher"
 end
 
 group :development do
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   validates :password, format: { with: /[\!@#\$%\^&\*]/, message: "must include at least one special character" }, on: :create
-
 end
