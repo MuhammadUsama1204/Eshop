84036e78baf66757188dcae0306c08ec6486e6c1
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-25 12:00:47 +0500

    [105]add create products functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,19 @@
 class ProductsController < ApplicationController
+
+    def new
+        @product = Product.new
+    end
+    def create
+        @product = Product.new(product_params)
+        if @product.save
+            flash[:success] = "Successfully Added"
+            redirect_to products_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    private
+    def product_params
+        params.require(:product).permit(:title, :description, :price, :total_quantity, :quantity_in_stock)
+
+    end
 end
