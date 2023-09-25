7dbc9f1e3ad2f068641b2d73e51f474024817bfd
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-25 17:44:30 +0500

    [105] fix error on migrations

--- db/schema.rb
+++ db/schema.rb
@@ -24,14 +24,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_062103) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string "role"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "username"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -42,20 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_062103) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "users_roles", force: :cascade do |t|
-    t.bigint "role_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["role_id"], name: "index_users_roles_on_role_id"
-    t.index ["user_id"], name: "index_users_roles_on_user_id"
-  end
-
-  add_foreign_key "users_roles", "roles", on_delete: :cascade
-  add_foreign_key "users_roles", "users", on_delete: :cascade
 end
