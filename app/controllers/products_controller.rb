31c4261dd89048d2f841b7f1bfa07f8e42f21074
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-25 15:59:30 +0500

    [105] add show(details of specific product)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,23 +1,26 @@
 class ProductsController < ApplicationController
 
-    def index
-        @products= Product.all
-    end
-    def new
-        @product = Product.new
-    end
-    def create
-        @product = Product.new(product_params)
-        if @product.save
-            flash[:success] = "Successfully Added"
-            redirect_to products_path
-        else
-            render :new, status: :unprocessable_entity
-        end
-    end
-    private
-    def product_params
-        params.require(:product).permit(:title, :description, :price, :total_quantity, :quantity_in_stock)
-    end
+  def index
+      @products= Product.all
+  end
+  def show
+      @product = Product.find(params[:id])
+  end
+  def new
+      @product = Product.new
+  end
+  def create
+      @product = Product.new(product_params)
+      if @product.save
+          flash[:success] = "Successfully Added"
+          redirect_to products_path
+      else
+          render :new, status: :unprocessable_entity
+      end
+  end
+  private
+  def product_params
+      params.require(:product).permit(:title, :description, :price, :total_quantity, :quantity_in_stock)
+  end
 
 end
