38bc555586c41f9be616f22915cdc8122523e9a7
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-25 18:12:28 +0500

    [105]added destroy action for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,12 +3,15 @@ class ProductsController < ApplicationController
   def index
       @products= Product.all
   end
+
   def show
       @product = Product.find(params[:id])
   end
+
   def new
       @product = Product.new
   end
+
   def create
       @product = Product.new(product_params)
       if @product.save
@@ -18,9 +21,11 @@ class ProductsController < ApplicationController
           render :new, status: :unprocessable_entity
       end
   end
+
   def edit
     @product = Product.find(params[:id])
   end
+
   def update
     @product = Product.find(params[:id])
     if @product.update(product_params)
@@ -30,6 +35,15 @@ class ProductsController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @product = Product.find(params[:id])
+        if @product.destroy
+            redirect_to root_path, notice: "Product was successfully destroyed."
+        else
+            redirect_to root_path, alert: "Unable to destroy Product."
+        end
+    end
   private
   def product_params
       params.require(:product).permit(:title, :description, :price, :total_quantity, :quantity_in_stock)
