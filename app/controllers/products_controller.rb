6e0fcd2c0f931daff261e9b12c1d60d57d2afdea
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-25 16:12:09 +0500

    [105] edit/update in controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,6 +18,18 @@ class ProductsController < ApplicationController
           render :new, status: :unprocessable_entity
       end
   end
+  def edit
+    @product = Product.find(params[:id])
+  end
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:success] = "Successfully Updated"
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
   private
   def product_params
       params.require(:product).permit(:title, :description, :price, :total_quantity, :quantity_in_stock)
