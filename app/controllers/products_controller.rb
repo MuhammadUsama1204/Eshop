4f7820156872006b55950178d6b7dd1fe7b26be2
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-26 18:22:02 +0500

    product:update searchFilter exceptional handling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -45,16 +45,32 @@ class ProductsController < ApplicationController
             redirect_to products_path, alert: "Unable to destroy Product."
         end
     end
+
   private
   def product_params
       params.require(:product).permit(:title, :description, :price, :total_quantity, :quantity_in_stock,:display_picture)
   end
 
   def searchFilter
-    @products = @products.where("title LIKE ?", "%#{params[:search]}%")
-    min_price = params[:min_price]
-    max_price = params[:max_price]
-    @products = Product.where(price: min_price..max_price)
+    if params[:search].present?
+      @products = @products.where("title ILIKE ?", "%#{params[:search]}%")
+    end
+
+    if params[:min_price].present? && params[:max_price].present?
+      min_price = params[:min_price]
+      max_price = params[:max_price]
+      @products = @products.where(price: min_price..max_price)
+    end
+
+    if params[:min_price].present?
+      min_price = params[:min_price]
+      @products = @products.where("price >= ?", min_price)
+    end
+
+    if params[:max_price].present?
+      max_price = params[:max_price]
+      @products = @products.where("price <= ?", max_price)
+    end
   end
 
 end
