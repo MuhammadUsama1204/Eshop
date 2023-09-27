3f37bc5363de2e7e669a478c94df006dd37973a0
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-27 15:13:57 +0500

    fix the duplicate migration,add products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_27_100407) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_101107) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price"
+    t.integer "total_quantity"
+    t.integer "quantity_in_stock"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "role"
