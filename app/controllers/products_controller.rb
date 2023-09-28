470537d5298044c83823062f20969efa8d55769d
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-28 14:03:35 +0500

    product:fix indentation and call search function in index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,29 +2,26 @@ class ProductsController < ApplicationController
   before_action :find_product, only: [:show, :edit, :update, :destroy]
 
   def index
-      @products= Product.all
-      searchFilter
+    @products = Product.searchFilter(params)
   end
 
-  def show
-  end
+  def show; end
 
   def new
-      @product = Product.new
+    @product = Product.new
   end
 
   def create
     @product = Product.new(product_params)
     if @product.save
-        flash[:success] = "Successfully Added"
-        redirect_to products_path
+      flash[:success] = "Successfully Added"
+      redirect_to products_path
     else
-        render :new, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @product.update(product_params)
@@ -46,29 +43,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-      params.require(:product).permit(:title, :description, :price, :total_quantity, :quantity_in_stock,:display_picture)
-  end
-
-  def searchFilter
-    if params[:search].present?
-      @products = @products.where("title ILIKE ?", "%#{params[:search]}%")
-    end
-
-    if params[:min_price].present? && params[:max_price].present?
-      min_price = params[:min_price]
-      max_price = params[:max_price]
-      @products = @products.where(price: min_price..max_price)
-    end
-
-    if params[:min_price].present?
-      min_price = params[:min_price]
-      @products = @products.where("price >= ?", min_price)
-    end
-
-    if params[:max_price].present?
-      max_price = params[:max_price]
-      @products = @products.where("price <= ?", max_price)
-    end
+    params.require(:product).permit(:title, :description, :price, :total_quantity, :quantity_in_stock,:display_picture)
   end
 
   def find_product
