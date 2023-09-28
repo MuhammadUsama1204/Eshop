a9edcff6a4ee9e7c856f10d177259a3af6cdaff4
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-09-28 14:02:53 +0500

    product:made function for search in model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
   has_one_attached :display_picture
+  
+  def self.searchFilter(params)
+    @products = Product.all
+    @products = @products.where("title ILIKE ?", "%#{params[:search]}%") if params[:search].present?
+    @products = @products.where("price >= ?", params[:min_price]) if params[:min_price].present?
+    @products = @products.where("price <= ?", params[:max_price]) if params[:max_price].present?
+    @products
+  end
 end
