dabb679a71ef26abcf5b56c6aeb3ec7b70d7e60c
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-10-03 16:51:14 +0500

    product:fix search filteration

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,11 +3,10 @@ class Product < ApplicationRecord
   has_many :carts, through: :line_items
   has_one_attached :display_picture
   PER_PAGE = 10;
-  def self.searchFilter(params)
-    @products = Product.all
-    @products = @products.where("title ILIKE ?", "%#{params[:search]}%") if params[:search].present?
-    @products = @products.where("price >= ?", params[:min_price]) if params[:min_price].present?
-    @products = @products.where("price <= ?", params[:max_price]) if params[:max_price].present?
-    @products
+  def self.searchFilter(products, params)
+    products = products.where("title ILIKE ?", "%#{params[:search]}%") if params[:search].present?
+    products = products.where("price >= ?", params[:min_price]) if params[:min_price].present?
+    products = products.where("price <= ?", params[:max_price]) if params[:max_price].present?
+    products
   end
 end
