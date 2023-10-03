c4563c288be3c6574fbd51814384a7da4f6ab954
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-10-03 16:50:32 +0500

    user:fix pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,7 @@ class UsersController < ApplicationController
   before_action :authorize_user, only: %i[destroy edit update]
 
   def index
-    @users = User.all
-    @paginate_users = User.paginate(page: params[:page], per_page: User::PER_PAGE)
+    @users = User.all.paginate(page: params[:page], per_page: User::PER_PAGE)
   end
 
   def new
