6bf2eeb95208c1a6ea93c65fa8f0cea219134f5a
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-10-04 17:05:29 +0500

    cart: modify set_user_cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,24 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-	before_action :set_user_cart_
+	before_action :set_user_cart
+
+  private
+
+  def set_user_cart
+    if user_signed_in?
+      if current_user.cart
+        @cart = current_user.cart
+      else
+        @cart = current_user.create_cart
+      end
+    else
+      @cart = nil 
+    end
+  end
 
   protected
 
   def after_sign_in_path_for(resource)
     products_path
   end
-  
-  private
-
-	def set_user_cart_
-    if current_user.cart
-      @cart = current_user.cart
-    else
-      @cart = current_user.create_cart
-    end
-	end
 end
