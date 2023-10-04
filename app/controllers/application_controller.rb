fc0bb4979f6cc07890f856435ba9f5a5f845f524
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-10-04 11:50:14 +0500

    cart: set cart for every new user to access whole application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+	before_action :set_user_cart_
 
   protected
 
   def after_sign_in_path_for(resource)
     products_path
   end
+  
+  private
+
+	def set_user_cart_
+    if current_user.cart
+      @cart = current_user.cart
+    else
+      @cart = current_user.create_cart
+    end
+	end
 end
