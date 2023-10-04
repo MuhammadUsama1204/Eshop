8eb882964d02e96242e0e2b727e1d7b50d121cda
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-10-04 13:46:37 +0500

    cart:add_to_cart update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations',
   }
-  resources :users do
-  post '/carts/add_to_cart/:product_id', to: 'carts#add_to_cart', as: 'add_to_cart'
-end
+  resources :users  
   resources :products
   resources :carts 
   resources :line_items 
+  post '/carts/add_to_cart/:product_id', to: 'carts#add_to_cart', as: 'add_to_cart'
+
 end
