7d6299dcc373da4b9d08facd5c94a3a50f1858a2
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-10-09 19:58:03 +0500

    product:add validation for quantity in stock non negative

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   has_many :line_items, dependent: :destroy
   has_many :carts, through: :line_items
   has_one_attached :display_picture
+  validates :quantity_in_stock, numericality: { greater_than_or_equal_to: 0, message: "must be a non-negative number"}, on: [:create, :update]
   PER_PAGE = 10;
   def self.searchFilter(products, params)
     products = products.where("title ILIKE ?", "%#{params[:search]}%") if params[:search].present?
