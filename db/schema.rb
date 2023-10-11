f3e962f25b8db05b46f4a37046c2299e468349d2
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-10-11 15:56:50 +0500

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_03_095209) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_11_104640) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,6 +71,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_03_095209) do
     t.bigint "cart_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "billing_address"
+    t.text "shipping_address"
+    t.decimal "sub_total"
     t.index ["cart_id"], name: "index_orders_on_cart_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
