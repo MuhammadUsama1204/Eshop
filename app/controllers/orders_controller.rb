8f95f0d4d756075a70590edcf1473c14c5e97c67
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-10-12 18:01:00 +0500

    order: create edit&update action for order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,19 @@ class OrdersController < ApplicationController
     @order.user = current_user 
     @order.cart = current_user.cart 
   end
+  
+  def edit
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to orders_path, notice: 'Order was successfully updated.'
+    else
+      render 'edit'
+    end
+  end
 
   def create
     @order = Order.new(order_params)
