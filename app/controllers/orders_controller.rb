01dcef1f7e7afc0315cc76edad66d516e622ffb6
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-10-12 12:43:36 +0500

    order:update new action and made create action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,17 @@ class OrdersController < ApplicationController
 
   def new 
     @order = Order.new
+    @order.user = current_user 
+    @order.cart = current_user.cart 
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      render 'new'
+    end
   end
 
 end
