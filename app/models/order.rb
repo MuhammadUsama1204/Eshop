0f88cadc9cf6bed91d547624489835ee26e8d0d9
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-10-12 12:48:29 +0500

    create before_action for order_number assign and unique

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,13 @@
 class Order < ApplicationRecord
+  before_create :generate_order_number
+
   belongs_to :user
   belongs_to :cart
+
+  def generate_order_number
+    while true
+      self.order_number = "#{Time.current.strftime('%Y%m%d%H%M%S')}#{rand(1000..9999)}"
+      break unless Order.exists?(order_number: self.order_number)
+    end
+  end    
 end
