082c12d2ef36b049c99778f8a52081ff3364b148
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-10-16 18:42:40 +0500

    checked for assigning products true

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
+  let(:product) { create(:product) }
+
   context 'Index' do
     it 'returns true' do
       get :index
       expect(response).to be_successful
+    end
+
+    it 'assigns products' do
+      get :index
+      expect(assigns(:products)).to eq([product])
     end
   end
 end
