8c7e354fb8b7c9631cb27b2c6a3da55058313d10
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-10-17 19:54:17 +0500

    update api:product which have quantity_in_stock>0

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.where('quantity_in_stock > 0')
     render json: @products, status: 200
   end
 end
