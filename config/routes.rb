5966f1085cde4391f08b6680cf143d0aa1b27f9a
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-10-17 18:29:24 +0500

    routes for api only index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :products, only: %i[index]
+    end
+  end
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
