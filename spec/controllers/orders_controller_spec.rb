40ef7b234d7099f0b84df32a46b3ec2b99f43911
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-10-18 17:32:50 +0500

    add sign_in user for order creation rspec

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe OrdersController, type: :controller do
+  
+  let(:user) { FactoryBot.create(:user, confirmed_at: Time.now, roles: [create(:role, role: 'Customer')]) }
+
+  before do
+    sign_in user
+  end
+
   context 'with valid attributes' do
     it 'creates a new order' do
-      user = create(:user, roles: [create(:role, role: 'Customer')])
-      cart = create(:cart)
+      cart = create(:cart,user: user)
       expect do
         post :create, params: { order: attributes_for(:order, cart_id: cart.id, user_id: user.id) }
       end.to change(Order, :count).by(1)
     end
   end
+
 end
