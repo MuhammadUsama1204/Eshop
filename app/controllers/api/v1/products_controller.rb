3888d2c760cde1c07a842ca1b49563d039f6291c
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-10-18 13:35:24 +0500

    skip authorization for testing

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::ProductsController < ApplicationController
+  skip_before_action :authenticate_user!
+  
   def index
     @products = Product.where('quantity_in_stock > 0')
     render json: @products, status: 200
