8fdea7b0ac7b0465012d2ebfd2b5e09a359ae5fb
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-11-02 19:14:42 +0500

    update routes for apis

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :products, only: %i[index]
+      resources :products, only: %i[index destroy create update show]
     end
   end
   devise_for :users, controllers: {
